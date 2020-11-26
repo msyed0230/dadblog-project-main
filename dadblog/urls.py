d77e929b7a143a311e0b2f97a4f9c675d977ec17
msyed0230 <66385123+msyed0230@users.noreply.github.com>
2020-11-26 13:33:03 -0500

    About page active

--- dadblog/urls.py
+++ dadblog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
     path('blog/', include('blog.urls')),
+    path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
